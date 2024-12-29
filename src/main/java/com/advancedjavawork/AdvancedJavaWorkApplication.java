139ec753717a33ecd4ece739c727c8940fbc2586
qihui <1600587398@qq.com>
2024-12-29 18:00:06 +0800

    完成项目初始化。

---
+++ src/main/java/com/advancedjavawork/AdvancedJavaWorkApplication.java
@@ -0,0 +1,13 @@
+package com.advancedjavawork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJavaWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedJavaWorkApplication.class, args);
+    }
+
+}
