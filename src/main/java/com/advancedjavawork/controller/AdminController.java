7a80b876611fb817471c32ca51871996bd5cff87
qihui <1600587398@qq.com>
2025-01-08 12:10:39 +0800

    完成管理员登录。

--- src/main/java/com/advancedjavawork/controller/AdminController.java
+++ src/main/java/com/advancedjavawork/controller/AdminController.java
@@ -1,5 +1,10 @@
 package com.advancedjavawork.controller;
 
+import com.advancedjavawork.entity.Admin;
+import com.advancedjavawork.service.IAdminService;
+import com.advancedjavawork.utils.Result;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Resource
+    private IAdminService adminService;
 
+    @PostMapping("/login")
+    public Result login(Admin admin){
+        if (admin.getAdminName() == null || admin.getAdminPassword() == null) return Result.fail();
+        int result = adminService.login(admin);
+        if (result == 0) Result.fail();
+        return Result.success();
+    }
 }
--- src/main/java/com/advancedjavawork/service/impl/AdminServiceImpl.java
+++ src/main/java/com/advancedjavawork/service/impl/AdminServiceImpl.java
@@ -3,7 +3,9 @@ package com.advancedjavawork.service.impl;
 import com.advancedjavawork.entity.Admin;
 import com.advancedjavawork.mapper.AdminMapper;
 import com.advancedjavawork.service.IAdminService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +18,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements IAdminService {
-
+    @Resource
+    private AdminMapper mapper;
+    @Override
+    public int login(Admin admin) {
+        QueryWrapper<Admin> wrapper = new QueryWrapper<>();
+        wrapper.eq("admin_name",admin.getAdminName());
+        wrapper.eq("admin_password",admin.getAdminPassword());
+        Admin result = mapper.selectOne(wrapper);
+        if (result == null) return 0;
+        return 1;
+    }
 }
